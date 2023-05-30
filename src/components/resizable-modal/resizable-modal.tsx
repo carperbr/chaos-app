6179b17e1c0a445df0e641d5a479426b24cf7c6b
carperbr <carperbr@gmail.com>
2023-05-30 02:10:21 -0400

    adding minHeight/minWidth for windows

--- src/components/resizable-modal/resizable-modal.tsx
+++ src/components/resizable-modal/resizable-modal.tsx
@@ -5,12 +5,16 @@ import './resizable-modal.scss';
 interface ResizableModalProps {
     title: string;
     minHeight?: number;
+    minWidth?: number;
     isOpen: boolean;
     children: ReactNode;
 }
 
 export const ResizableModal = (props: ResizableModalProps) => {
     const [modalSize, setModalSize] = useState({ width: 400, height: 300 });
+
+    const minHeight = props.minHeight !== undefined ? props.minHeight : 64;
+    const minWidth = props.minWidth !== undefined ? props.minWidth : 128;
 
     const handleResize = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.preventDefault();
@@ -20,8 +24,8 @@ export const ResizableModal = (props: ResizableModalProps) => {
         
         const onMouseMove = (e: MouseEvent) => {
             setModalSize({
-                width: modalSize.width + (e.clientX - ax),
-                height: modalSize.height + (e.clientY - ay)
+                width: Math.max(minWidth, modalSize.width + (e.clientX - ax)),
+                height: Math.max(minHeight, modalSize.height + (e.clientY - ay))
             });
         }
 
