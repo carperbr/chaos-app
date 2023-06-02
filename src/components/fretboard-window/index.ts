bf496e6de6576f173e1eebfee978cbdc434fd948
carperbr <carperbr@gmail.com>
2023-06-02 05:09:55 -0400

    removing unused component

--- src/components/fretboard-window/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./fretboard-window";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./pitch-class";
 export * from "./tone-circle";
-export * from './fretboard-window';
 export * from './window';
 export * from './fretboard';
