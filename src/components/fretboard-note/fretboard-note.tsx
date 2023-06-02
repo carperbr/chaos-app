d213667591d28cac3ef880e27c94c327b8af64b3
carperbr <carperbr@gmail.com>
2023-06-02 05:44:05 -0400

    adding some quick code to rename windows for organization

--- src/components/fretboard-note/fretboard-note.tsx
+++ src/components/fretboard-note/fretboard-note.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { PitchUtils } from "../../utils";
 import "./fretboard-note.scss";
 
--- src/components/window/window.tsx
+++ src/components/window/window.tsx
@@ -1,7 +1,8 @@
-import React, { useState, ReactNode, CSSProperties } from "react";
+import React, { useState, useRef, ReactNode, CSSProperties } from "react";
 import Draggable from "react-draggable";
 import { WindowContext } from "./window-context";
 import { CloseButton } from "react-bootstrap";
+import { Overlay, Dropdown, DropdownButton, Popover } from "react-bootstrap";
 import "./window.scss";
 
 export interface WindowProps {
@@ -13,13 +14,18 @@ export interface WindowProps {
 }
 
 export const Window = (props: WindowProps) => {
-  const [modalSize, setModalSize] = useState({ width: 400, height: 300 });
-
   const onClose = props.onClose;
   const minHeight = props.minHeight !== undefined ? props.minHeight : 64;
   const minWidth = props.minWidth !== undefined ? props.minWidth : 128;
 
-  const title = props.title ? props.title : "New Window";
+  const [modalSize, setModalSize] = useState({ width: 400, height: 300 });
+  const target = useRef<SVGTextElement>(null);
+  const popover = useRef<HTMLDivElement>(null);
+  const input = useRef<HTMLInputElement>(null);
+  const [showTitleEdit, setShowTitleEdit] = useState(false);
+
+  const [title, setTitle] = useState(props.title ? props.title : "New Window")
+  const [currTitle, setCurrTitle] = useState(props.title ? props.title : "New Window")
 
   const handleResize = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
@@ -86,17 +92,47 @@ export const Window = (props: WindowProps) => {
     window.addEventListener("mouseup", onMouseUp);
   };
 
+  const handleClickOutisdeTitle = (event: MouseEvent) => {
+    if (popover.current && !popover.current.contains(event.target as Node)) {
+      setShowTitleEdit(false);
+      document.removeEventListener("mousedown", handleClickOutisdeTitle);
+    }
+  };
+
+  const handleClickTitle = () => {
+    if (!showTitleEdit) {
+      setCurrTitle(title)
+      setShowTitleEdit(true);
+      document.addEventListener("mousedown", handleClickOutisdeTitle);
+
+      setTimeout(() => {
+        if (input.current) {
+          input.current.focus();
+        }
+      }, 50)
+
+    }
+  };
+
+  const handleTitleKey = (e: React.KeyboardEvent<any>) => {
+    if (e.key.toLowerCase() == 'enter') {
+      setTitle(currTitle)
+      setShowTitleEdit(false)
+    }
+  }
+
   const modalStyle: CSSProperties = {
     width: modalSize.width,
     height: modalSize.height,
   };
 
   return (
+    <>
     <div>
       <Draggable handle=".handle" bounds=".app">
         <div className="window-background" style={modalStyle}>
           <div className={"handle"}>
-            <b className="title">{title}</b>
+            <text ref={target}><b className="title" onClick={() => handleClickTitle()}>{title}</b></text>
 
             <CloseButton className="close-handle" onClick={() => onClose()} />
           </div>
@@ -125,5 +161,15 @@ export const Window = (props: WindowProps) => {
         </div>
       </Draggable>
     </div>
+
+      <Overlay target={target.current} show={showTitleEdit} placement="top">
+        <Popover>
+          <Popover.Header as="h3">Change Note</Popover.Header>
+          <Popover.Body ref={popover}>
+            <input ref={input} value={currTitle} onChange={(e) => setCurrTitle(e.target.value)} onKeyDown={(e) => handleTitleKey(e)} />
+          </Popover.Body>
+        </Popover>
+      </Overlay>
+    </>
   );
 };
