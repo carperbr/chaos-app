f08af2d7da68cd49c2e9e1602ec268a5b158ed4c
carperbr <carperbr@gmail.com>
2023-06-03 22:51:17 -0400

    updates to allow configuring pitch sets

--- src/components/pitch-class/pitch-class.tsx
+++ src/components/pitch-class/pitch-class.tsx
@@ -2,19 +2,31 @@ import React, { useState, useRef } from "react";
 import { Overlay, Dropdown, DropdownButton, Popover } from "react-bootstrap";
 import "./pitch-class.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faCircleArrowLeft,
+  faCircleArrowDown,
+  faCircleArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 import { NOTES } from "../../utils";
 
 export interface PitchClassProps {
+  notes: string[];
   note: string;
   idx: number;
   x: number;
   y: number;
   selected: boolean;
   handleNoteChange: (idx: number, note: string) => void;
+  handleNoteRemoved: (idx: number) => void;
+  handleNoteAdded: (idx: number, side: number, note: string) => void;
 }
 
 export const PitchClass = (props: PitchClassProps) => {
   const [show, setShow] = useState(false);
+  const [insertNote, setInsertNote] = useState(props.note);
   const target = useRef<SVGTextElement>(null);
   const popover = useRef<HTMLDivElement>(null);
   const { note, x, y } = props;
@@ -28,13 +40,24 @@ export const PitchClass = (props: PitchClassProps) => {
 
   const handleClick = () => {
     if (!show) {
+      setInsertNote(props.note);
       setShow(true);
       document.addEventListener("mousedown", handleClickOutisde);
     }
   };
 
-  const handleSelection = (note: string) => {
+  const handleNoteChange = (idx: number, note: string) => {
     props.handleNoteChange(props.idx, note);
+    setShow(false);
+  };
+
+  const handleNoteRemoved = (idx: number) => {
+    props.handleNoteRemoved(idx);
+    setShow(false);
+  };
+
+  const handleNoteAdded = (idx: number, side: number, note: string) => {
+    props.handleNoteAdded(idx, side, note);
     setShow(false);
   };
 
@@ -56,17 +79,41 @@ export const PitchClass = (props: PitchClassProps) => {
         <Popover>
           <Popover.Header as="h3">Change Note</Popover.Header>
           <Popover.Body ref={popover}>
-            <DropdownButton title={note} variant="Primary">
-              {NOTES.map((n, idx) => (
-                <Dropdown.Item
-                  eventKey={idx}
-                  active={n === props.note}
-                  onClick={() => handleSelection(n)}
-                >
-                  {n}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
+            <div style={{ display: "flex" }}>
+              <DropdownButton title={insertNote} variant="Primary">
+                {NOTES.map((n, idx) => (
+                  <Dropdown.Item
+                    eventKey={idx}
+                    active={n === insertNote}
+                    onClick={() => setInsertNote(n)}
+                  >
+                    {n}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+              <FontAwesomeIcon
+                className="trash-icon"
+                icon={faCircleArrowDown}
+                onClick={() => handleNoteChange(props.idx, insertNote)}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                className="trash-icon"
+                icon={faCircleArrowLeft}
+                onClick={() => handleNoteAdded(props.idx, 0, insertNote)}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                className="trash-icon"
+                icon={faCircleArrowRight}
+                onClick={() => handleNoteAdded(props.idx + 1, 0, insertNote)}
+              ></FontAwesomeIcon>
+              {props.notes.length > 1 && (
+                <FontAwesomeIcon
+                  className="trash-icon"
+                  icon={faTrash}
+                  onClick={() => handleNoteRemoved(props.idx)}
+                ></FontAwesomeIcon>
+              )}
+            </div>
           </Popover.Body>
         </Popover>
       </Overlay>
--- src/components/tone-circle/tone-circle.tsx
+++ src/components/tone-circle/tone-circle.tsx
@@ -1,40 +1,84 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import "./tone-circle.scss";
 
 import { PitchClass } from "../pitch-class";
 import { NOTES } from "../../common";
 import { WindowContext } from "../window";
+import { Overlay, Popover, DropdownButton, Dropdown } from "react-bootstrap";
 
 export const ToneCircle = () => {
   const modalSize = useContext(WindowContext);
   const [activeIndex, setActiveIndex] = useState<number | null>();
   const [notes, setNotes] = useState<string[]>([...NOTES]);
+  const [showMenu, setShowMenu] = useState(false);
+  const target = useRef<SVGCircleElement>(null);
+  const popover = useRef<HTMLDivElement>(null);
 
   const handleNoteSelected = (idx: number, newNote: string) => {
     setNotes(notes.map((v, i) => (idx === i ? newNote : v)));
     setActiveIndex(null);
   };
 
+  const handleClickOutisde = (event: MouseEvent) => {
+    if (popover.current && !popover.current.contains(event.target as Node)) {
+      setShowMenu(false);
+      document.removeEventListener("mousedown", handleClickOutisde);
+    }
+  };
+
+  const handleClick = () => {
+    if (!showMenu) {
+      setShowMenu(true);
+      document.addEventListener("mousedown", handleClickOutisde);
+    }
+  };
+
+  const handleNoteRemoved = (idx: number) => {
+    setNotes(
+      notes.filter((n, i) => {
+        return i !== idx;
+      })
+    );
+  };
+
+  const handleNoteAdded = (idx: number, side: number, note: string) => {
+    let newNotes = [...notes, notes[0]].map((v, i) => {
+      if (i < idx) {
+        return v;
+      } else if (i == idx) {
+        return note;
+      } else {
+        return notes[i - 1];
+      }
+    });
+
+    setNotes(newNotes);
+  };
+
   const outerRadius = Math.min(modalSize.width, modalSize.height) * 0.4;
   const innerRadius = Math.min(modalSize.width, modalSize.height) * 0.3;
+  const textRadius = (outerRadius + innerRadius) * 0.5;
+  const menuRadius = Math.min(modalSize.width, modalSize.height) * 0.05;
 
   const circleNotes = notes.map((note, i) => {
-    const angle = (i / notes.length) * 2 * Math.PI;
+    const angle1 = (i / notes.length) * 2 * Math.PI;
+    const angle = angle1;
     const x =
-      modalSize.width / 2 +
-      (innerRadius + outerRadius) * -0.5 * Math.sin(-angle);
+      modalSize.width / 2 + textRadius * Math.cos(angle - Math.PI * 0.5);
     const y =
-      modalSize.height / 2 +
-      (innerRadius + outerRadius) * -0.5 * Math.cos(-angle);
+      modalSize.height / 2 + textRadius * Math.sin(angle - Math.PI * 0.5);
 
     return (
       <PitchClass
+        notes={notes}
         key={i}
         note={note}
         x={x}
         y={y}
         idx={i}
         handleNoteChange={handleNoteSelected}
+        handleNoteRemoved={handleNoteRemoved}
+        handleNoteAdded={handleNoteAdded}
         selected={i === activeIndex}
       />
     );
@@ -44,10 +88,12 @@ export const ToneCircle = () => {
     const angle1 = (i / notes.length) * 2 * Math.PI;
     const angle2 = ((i + 1) / notes.length) * 2 * Math.PI;
     const angle = (angle1 + angle2) * 0.5;
-    const x1 = modalSize.width / 2 + outerRadius * Math.cos(angle);
-    const y1 = modalSize.height / 2 + outerRadius * Math.sin(angle);
-    const x2 = modalSize.width / 2 + 0.5 * Math.cos(angle);
-    const y2 = modalSize.height / 2 + 0.5 * Math.sin(angle);
+    const x1 =
+      modalSize.width / 2 + outerRadius * Math.cos(angle - Math.PI * 0.5);
+    const y1 =
+      modalSize.height / 2 + outerRadius * Math.sin(angle - Math.PI * 0.5);
+    const x2 = modalSize.width / 2 + 0.5 * Math.cos(angle - Math.PI * 0.5);
+    const y2 = modalSize.height / 2 + 0.5 * Math.sin(angle - Math.PI * 0.5);
     return <line key={i} x1={x1} y1={y1} x2={x2} y2={y2} stroke="black" />;
   });
 
@@ -71,8 +117,23 @@ export const ToneCircle = () => {
           />
           {lines}
           {circleNotes}
+          <circle
+            className="center-menu"
+            cx={modalSize.width / 2}
+            cy={modalSize.height / 2}
+            r={menuRadius}
+            onClick={() => handleClick()}
+            ref={target}
+          />
         </svg>
       </div>
+
+      <Overlay target={target.current} show={showMenu} placement="bottom">
+        <Popover>
+          <Popover.Header as="h3">Edit Set</Popover.Header>
+          <Popover.Body ref={popover}>Menu</Popover.Body>
+        </Popover>
+      </Overlay>
     </>
   );
 };
